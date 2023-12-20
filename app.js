038c1b821a6869689ca7b4721291f7982a072b70
antorahad <antorahad6@gmail.com>
2023-12-20 12:43:52 +0600

    navbar & sidebar

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+const profile_Btn = document.getElementById('profilePannel');
+const profile_Menu = document.getElementById('profileMenu');
+let profileMenuToggle = false;
 const side_Btn = document.getElementById('sideBtn');
 const side_Menu = document.getElementById('sideMenu');
 let sideMenuToggle = false;
+
+profile_Btn.addEventListener('click', () => {
+  if(!profileMenuToggle){
+    profile_Menu.classList.add('show');
+    profileMenuToggle = true;
+  }else{
+    profile_Menu.classList.remove('show');
+    profileMenuToggle = false;
+  }
+})
 
 side_Btn.addEventListener('click', () => {
   // Toggle the 'show' class on side_Menu
@@ -12,3 +25,4 @@ side_Btn.addEventListener('click', () => {
     sideMenuToggle = false;
   }
 });
+
