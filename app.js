760aa8a8e14aebf4dac66afbddc2ded14ee8dde5
antorahad <antorahad6@gmail.com>
2023-12-20 14:43:06 +0600

    working on providers design

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ let profileMenuToggle = false;
 const side_Btn = document.getElementById('sideBtn');
 const side_Menu = document.getElementById('sideMenu');
 let sideMenuToggle = false;
+const provider_Btn = document.getElementById('providerDropDown');
+const provider_Container = document.getElementById('providerContainer');
+let providerMenuToggle = false;
 
 profile_Btn.addEventListener('click', () => {
   if(!profileMenuToggle){
@@ -23,6 +26,17 @@ side_Btn.addEventListener('click', () => {
   } else {
     side_Menu.classList.remove('show');
     sideMenuToggle = false;
+  }
+});
+
+provider_Btn.addEventListener('click', () => {
+  // Toggle the 'show' class on side_Menu
+  if (!providerMenuToggle) {
+    provider_Container.classList.add('show');
+    providerMenuToggle = true;
+  } else {
+    provider_Container.classList.remove('show');
+    providerMenuToggle = false;
   }
 });
 
